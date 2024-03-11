386cd060f0ccec940b1a20aea928330db4fda68d
Oliver Crothers <olivercrothers77@gmail.com>
2024-03-11 11:56:13 +0000

    Basic setup

--- src/Application.java
+++ src/Application.java
@@ -1,22 +1,58 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Application {
 
     public static void main(String[] args){
         Application app = new Application();
-        app.menu();
+        ArrayList<Task> tasks = new ArrayList<>();
+        ArrayList<Task> completedTasks = new ArrayList<>();
+        app.menu(tasks, completedTasks);
     }
 
-    public static void createTask(){
-        System.out.println("I have been sent here for some reason");
+    public void checkTaskOff(ArrayList<Task> task, ArrayList<Task> completedTask){
+        System.out.println("Index of task: ");
+        Scanner index = new Scanner(System.in);
+        int taskIndex = index.nextInt();
+        Task movedtask = task.get(taskIndex);
+        completedTask.add(movedtask);
+        task.remove(taskIndex);
     }
-    public static void menu(){
-        System.out.println("Select an option: N = new task");
+
+    public void getTasks(ArrayList<Task> task){
+        for(int i=0; i<task.size(); i++){
+            System.out.println(" " + i + ". " + task.get(i));
+        }
+    }
+
+    public void createTask(ArrayList<Task> task){
+        String taskName;
+        do{
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Enter Task Name and priority(1-3): ");
+            taskName = scan.nextLine();
+            int priority = scan.nextInt();
+            task.add(new Task(taskName, priority, false));
+        }while(!taskName.equals("exit"));
+        task.removeLast();
+    }
+    public void menu(ArrayList<Task> tasks, ArrayList<Task> completedTasks){
+        System.out.println("Select an option: N = new task, M = see tasks, C = Check task off");
         Scanner input = new Scanner(System.in);
         String option = input.nextLine().toUpperCase();
         switch(option){
             case "N":
-                createTask();
+                createTask(tasks);
+                System.out.println(tasks.toString());
+                menu(tasks, completedTasks);
                 break;
+            case "M":
+                getTasks(tasks);
+                menu(tasks, completedTasks);
+                break;
+            case "C":
+                checkTaskOff(tasks, completedTasks);
+                System.out.println(completedTasks.toString());
+                menu(tasks, completedTasks);
             default:
                 break;
         }
